9056b058ea41156a7621e71dbedff90fc21ed052
Shirinzoy <rahim.shirinzoy@gmail.com>
2022-01-04 01:01:09 -0500

    this is my first repo

--- src/Git.java
+++ src/Git.java
@@ -4,6 +4,12 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a = 4+5;
+		int b = 4-2;
+		
+		int sum = a +b;
+		
+		System.out.println(sum);
 	}
 
 }
